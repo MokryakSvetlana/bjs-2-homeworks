f5657dfdc2a67ca4a33c440093209765e15cc548
msvetlana <s.ruzanova92@gmail.com>
2024-03-02 12:23:34 +0300

    Homework 6exception

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+   value = Number.parseFloat(value);
+    if (Number.isNaN(value)) {
+       throw new Error("Невалидное значение");
+    }
+
+    return value;
+}
+
+function validateCount(value) {
+    try {
+       return parseCount(value);
+    } catch (error) {
+       return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    get perimeter() {
+       return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const halfPerimeter = this.perimeter / 2;
+        const s = Math.sqrt(halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c));
+        return Number.parseFloat(s.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+       return new Triangle(a, b, c);
+    } catch(error) {
+        return {
+           get area() {
+              return 'Ошибка! Треугольник не существует'
+           },
+           get perimeter() {
+               return 'Ошибка! Треугольник не существует'
+           }
+        }
+    }
+}
